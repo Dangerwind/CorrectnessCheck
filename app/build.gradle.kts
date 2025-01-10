18218116fd15867d207d6af2674895a9d995ca92
Dangerwind <dangerwind@gmail.com>
2025-01-10 14:25:22 +0700

    some fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,4 +20,8 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.jacocoTestReport { reports { xml.required.set(true) }}
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
+}
